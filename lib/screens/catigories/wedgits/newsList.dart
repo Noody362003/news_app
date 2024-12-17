900579c510f88da049db7d816d3e263145bbf5c9
Nada Yasser <nadayasser358@gmail.com>
2024-12-17 07:41:29 +0200

    stable version without news details and search part

---
+++ lib/screens/catigories/wedgits/newsList.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:news/api/api_services.dart';
+import 'package:news/screens/catigories/models/news_model.dart';
+
+import '../news_card.dart';
+
+class NewsList extends StatelessWidget {
+  const NewsList({super.key, required this.sourceId});
+  final String sourceId;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        children: [
+      FutureBuilder<NewsModel>(
+      future: ApiServices.getNews(sourceId),
+      builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+      return const Center(
+      child: CircularProgressIndicator(),
+      );
+      } else if (snapshot.hasError) {
+      return Center(
+      child: Text('Error: ${snapshot.error.toString()}'),
+      );
+      }
+      
+      NewsModel? newsModel = snapshot.data;
+      var newsList = newsModel?.articles ?? [];
+      
+      return ListView.builder(
+      shrinkWrap: true,
+      scrollDirection: Axis.vertical,
+      physics: const NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) => NewsCard(newsModel: newsList[index]),
+      itemCount: newsList.length,
+      );
+      },
+      ),
+        ],
+      ),
+    );
+  }
+}
